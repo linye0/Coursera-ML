52451f985366a9f8e5ba2ca7bb12af59d87eb67d
linye0 <2629442736@qq.com>
2023-04-23 23:28:59 +0800

    ex7

--- code/ex7-kmeans and PCA/ex7.py
+++ code/ex7-kmeans and PCA/ex7.py
@@ -5,12 +5,17 @@ Created on Sat Apr 22 21:09:30 2023
 @author: 26294
 """
 
+import warnings
+warnings.filterwarnings('ignore')
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 from scipy.io import loadmat
 from sklearn import svm
+from IPython.display import display, Image
+from skimage import io
 
 def distance(x, y):
     return np.sum((x - y) ** 2)
@@ -41,8 +46,30 @@ def compute_centroids(X, idx, k):
         
     return computed_centroids
 
+def run_k_means(X, initial_centroids, max_iters):
+    m, n = X.shape
+    k = initial_centroids.shape[0]
+    centroids = initial_centroids
+    
+    for i in range(max_iters):
+        idx = find_closest_centroids(X, centroids)
+        centroids = compute_centroids(X, idx, k)
+        
+    return idx, centroids
+        
+def init_centroids(X, k):
+    m, n = X.shape
+    centroids = np.zeros((k, n))
+    idx = np.random.randint(0, m, k)
+    
+    for i in range(k):
+        centroids[i, :] = X[idx[i], :]
+    
+    return centroids
+
 if __name__ == '__main__':
     
+    """
     data = loadmat('data/ex7data2.mat')
 
     X = data['X']
@@ -50,9 +77,57 @@ if __name__ == '__main__':
     initial_centroids = np.array([[3, 3], [6, 2], [8, 5]])
     
     closest_centroids = find_closest_centroids(X, initial_centroids)
-
-    print(closest_centroids)
     
-    computed_centroids = compute_centroids(X, closest_centroids, 3)
+    idx, centroids = run_k_means(X, initial_centroids, 1000)
     
-    print(computed_centroids)
+    print(centroids)
+    
+    random_initial_centroids = init_centroids(X, 3)
+    
+    rndidx, rndcentroids = run_k_means(X, random_initial_centroids, 1000)
+    
+    print(rndcentroids)
+    
+    data2 = pd.DataFrame(data.get('X'), columns=['X1', 'X2'])
+    data2.head()
+    
+    sb.set(context="notebook", style="white")
+    sb.lmplot('X1', 'X2', data=data2, fit_reg=False)
+    plt.show()
+    
+    cluster1 = X[np.where(idx == 0)[0], :]
+    cluster2 = X[np.where(idx == 1)[0], :]
+    cluster3 = X[np.where(idx == 2)[0], :]
+    
+    fig, ax = plt.subplots(figsize = (12, 8))
+    ax.scatter(cluster1[:, 0], cluster1[:, 1], color = 'r', s = 30, label = 'Cluster1')
+    ax.scatter(cluster2[:, 0], cluster2[:, 1], color = 'g', s = 30, label = 'Cluster2')
+    ax.scatter(cluster3[:, 0], cluster3[:, 1], color = 'b', s = 30, label = 'Cluster3')
+    """
+    
+    """
+    img = Image(filename = 'data/bird_small.png')
+    display(img)
+    
+    image_data = loadmat(file_name = 'data/bird_small.mat')
+    A = image_data['A']
+    print(A.shape)
+    
+    A = A / 255
+    
+    X = np.reshape(A, (A.shape[0] * A.shape[1], A.shape[2]))
+    
+    initial_centroids = init_centroids(X, 16)
+    
+    idx, centroids = run_k_means(X, initial_centroids, 10)
+    
+    idx = find_closest_centroids(X, centroids)
+    
+    X_recovered = centroids[idx.astype(int), :]
+    
+    X_recovered = np.reshape(X_recovered, (A.shape[0], A.shape[1], A.shape[2]))
+    
+    plt.imshow(X_recovered)
+    
+    plt.show()
+    """
