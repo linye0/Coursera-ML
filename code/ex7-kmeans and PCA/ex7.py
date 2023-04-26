78f65a923493d4a841106086756d9f097837003f
linye0 <2629442736@qq.com>
2023-04-26 20:40:40 +0800

    2023/4/26

--- code/ex7-kmeans and PCA/ex7.py
+++ code/ex7-kmeans and PCA/ex7.py
@@ -67,6 +67,43 @@ def init_centroids(X, k):
     
     return centroids
 
+def pca(X):
+    # normalize the features
+    X = (X - X.mean()) / X.std()
+    
+    # compute the covariance matrix
+    X = np.matrix(X)
+    cov = (X.T * X) / X.shape[0]
+    
+    # perform SVD
+    U, S, V = np.linalg.svd(cov, full_matrices=1)
+    
+    return U, S, V
+
+def project_data(X, U, k):
+    U_reduced = U[:, :k]
+    return np.dot(X, U_reduced)
+
+def recover_data(X, U, k):
+    U_reduced = U[:, :k]
+    return np.dot(X, U_reduced.T)
+
+def plot_n_image(X, n):
+    pic_size = int(np.sqrt(X.shape[1]))
+    grid_size = int(np.sqrt(n))
+    
+    frist_n_images = X[:n, :]
+    
+    fig, ax_array = plt.subplots(nrows = grid_size, ncols = grid_size, sharex = True, sharey = True, figsize = (8, 8))
+    
+    for r in range(grid_size):
+        for c in range(grid_size):
+            ax_array[r, c].imshow((frist_n_images[grid_size * r + c].reshape((pic_size, pic_size))).T)
+            plt.xticks(np.array([]))
+            plt.yticks(np.array([]))
+            
+    plt.show()
+
 if __name__ == '__main__':
     
     """
@@ -131,3 +168,55 @@ if __name__ == '__main__':
     
     plt.show()
     """
+    
+    data = loadmat('data/ex7data1.mat')
+    
+    X = data['X']
+    
+    fig, ax = plt.subplots(figsize = (12, 8))
+    
+    ax.scatter(X[:, 0], X[:, 1])
+    
+    plt.show()
+    
+    U, S, V = pca(X)
+    
+    Z = project_data(X, U, 1)
+    
+    X_recovered = recover_data(Z, U, 1)
+    
+    fig, ax = plt.subplots(figsize=(12,8))
+    
+    ax.scatter(X[:, 0], X[:, 1], color = 'g')
+    
+    ax.scatter(list(X_recovered[:, 0]), list(X_recovered[:, 1]))
+    
+    plt.show()
+    
+    faces = loadmat('data/ex7faces.mat')
+    
+    X = faces['X']
+    
+    plot_n_image(X, 100)
+    
+    plot_n_image(X, 4)
+    
+    U, S, V = pca(X)
+    
+    face = np.reshape(X[3, :], (32, 32))
+    
+    plt.imshow(face.T)
+    
+    plt.show()
+    
+    Z = project_data(X, U, 100)
+    
+    X_recovered = recover_data(Z, U, 100)
+    
+    face = np.reshape(X_recovered[3,:], (32, 32))
+    
+    plt.imshow(face.T)
+    
+    plt.show()
+    
+    plot_n_image(U.T, 25)
